58bc5f3c9ba4e91c86edf03cb0d503d91073d5c0
xvjau <xvjau@c7be1d08-1417-0410-99dc-ed190eba7aff>
2007-04-16 20:48:59 +0000

    Created proper linux dist dir

---
+++ rs/dist/linux/make-tar.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+FILES="Conf libfbclient.so.2.0.0 libicudata.so.30 libicui18n.so.30 libicuuc.so.30 rs.sh \
+libfbclient.so libib_util.so libicudata.so.30.0 libicui18n.so.30.0  libicuuc.so.30.0 rs_en.qm \
+libfbclient.so.2 libicudata.so libicui18n.so libicuuc.so rs rs_ptBR.qm"
+
+if [ "${1}" == "" ]; then
+	echo error: Version string missing
+else
+	tar cjvf rs-${1}-$(uname -m).tar.bz2 $FILES
+fi
